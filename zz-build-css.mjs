5f5a2aae8f395d11d6b6791e72e1bdb27cb7378f
nikolamirilo <nikolamirilo@gmail.com>
2026-08-09 20:34:49 +0200

    removed unused files

--- zz-build-css.mjs
+++
@@ -1,8 +0,0 @@
-import postcss from "postcss";
-import tw from "@tailwindcss/postcss";
-import { readFileSync, writeFileSync } from "node:fs";
-const from = "/home/nikola/Desktop/say-cheese/src/app/globals.css";
-const css = readFileSync(from, "utf8");
-const out = await postcss([tw()]).process(css, { from });
-writeFileSync(process.argv[2], out.css);
-console.log("css bytes", out.css.length);
--- zz-live/stub-image.tsx
+++
@@ -1,4 +0,0 @@
-export default function Image(props: Record<string, unknown>) {
-  // eslint-disable-next-line @next/next/no-img-element
-  return <img alt="" {...props} />;
-}
